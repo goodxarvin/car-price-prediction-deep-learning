b54d97de9e9516478e2f2d866b86195064418c6c
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-11-23 13:23:29 +0330

    normalized model with label encoder and kmeans=700

--- data/car_model/data_cleaning_model.py
+++ data/car_model/data_cleaning_model.py
@@ -1,10 +1,18 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
 
 df = pd.read_csv("data/csv_outputs/cleaned_mileage_data.csv")
 df["model"] = df["model"].apply(lambda x: x-621 if x > 1950 else x)
 df["model"] = df["model"].apply(lambda x: x/1404)
 
+
+model_encoder = LabelEncoder()
+df["model_id"] = model_encoder.fit_transform(df["model"])
+
 # df = pd.get_dummies(df, columns=["model"])
-df.columns = [col.replace("model_", "") for col in df.columns]
+# df.columns = [col.replace("model_", "") for col in df.columns]
+
+
 df.to_csv("data/csv_outputs/cleaned_mileage_model_data.csv",
           index=False, float_format="%.15f")
