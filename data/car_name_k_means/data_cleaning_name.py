a29e09ab626d4ac49a47ff5ff36aac1cf71d22e0
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-12-08 13:47:12 +0330

    cleaning data outliers prices for better training

--- data/car_name_k_means/data_cleaning_name.py
+++ data/car_name_k_means/data_cleaning_name.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from hazm import word_tokenize
 from data.car_name_k_means.cleaning_name_funcs import filter_unuseful_words
@@ -25,12 +26,31 @@ with open("data/car_name_k_means/cluster.txt", "w", encoding="utf-8") as file:
         file.write("\n")
 
 
-print(df.shape)
-df = df.dropna(subset="name")
-print(df.shape)
+clean_rows = []
 
-df.to_csv("data/csv_outputs/cleaned_mileage_model_price_name_data.csv",
-          float_format="%.15f", index=False)
+for cluster_id, group in df.groupby("name_cluster"):
+
+    q1 = group["price"].quantile(0.25)
+    q3 = group["price"].quantile(0.75)
+    iqr = q3 - q1
+
+    lower = q1 - 1.5 * iqr
+    upper = q3 + 1.5 * iqr
+
+    good = group[(group["price"] >= lower) &
+                 (group["price"] <= upper)]
+
+    clean_rows.append(good)
+
+df_clean = pd.concat(clean_rows, ignore_index=True)
+
+print("before cleaning: ", len(df))
+print("after cleaning: ", len(df_clean))
+print("number deleted: ", len(df) - len(df_clean))
+
+
+df_clean.to_csv("data/csv_outputs/cleaned_mileage_model_price_name_data.csv",
+                float_format="%.15f", index=False)
 
 
 # name_cluster_dummies = pd.get_dummies(df["name_cluster"])
