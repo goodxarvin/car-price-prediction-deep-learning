3481c0c3361daecb785b17c8a40ab3a262a6391b
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-12-20 16:17:54 +0330

    deleted some useless lines of code

--- data/car_price/data_cleaning_price.py
+++ data/car_price/data_cleaning_price.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("data/csv_outputs/cleaned_mileage_model_data.csv")
-df = df[df["price"] != 11111111]
+# df = df[df["price"] != 11_111_111]
 df = df[(df["price"] > 10_000_000) & (df["price"] < 10_000_000_000)]
 
 df["price_scaled"] = np.log1p(df["price"])
