1c93d790cf74af3482dc75500d56c47cd1de807c
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-12-21 03:01:12 +0330

    more info for error distribution file

--- error_analysis/distribution_error.py
+++ error_analysis/distribution_error.py
@@ -15,10 +15,13 @@ p50 = (df_errors["error"] < 50_000_000).mean() * 100
 
 p100 = (df_errors["error"] < 100_000_000).mean() * 100
 
+p120 = (df_errors["error"] < 150_000_000).mean() * 100
+
 p200 = (df_errors["error"] < 200_000_000).mean() * 100
 
 print("less then 50M:", round(p50, 2), "%")
 print("less then 100M:", round(p100, 2), "%")
+print("less then 150M:", round(p120, 2), "%")
 print("less then 200M:", round(p200, 2), "%")
 
 print("\nloss mean", int(df_errors["error"].mean()))
